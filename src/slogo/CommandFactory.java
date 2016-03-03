13161d0fcddca229f0ee7b812de7722c7aecf542
Anita Desai <anita.desai96@gmail.com>
2016-03-03 17:23:42 -0500

    GUI error resolved

--- src/slogo/CommandFactory.java
+++ src/slogo/CommandFactory.java
@@ -2,6 +2,7 @@ package slogo;
 
 import java.awt.List;
 import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 
 import commands.Command;
@@ -26,19 +27,39 @@ public class CommandFactory {
 			constant = Integer.parseInt(word);
 		}catch (NumberFormatException e){
 		}
-		try {
 			System.out.println("Creating " + word + " " + commandName);
-			Class commClass = Class.forName("commands." + commandName);
-			Constructor commConstructor = commClass.getConstructor(double.class);
-						CommandNode command =  (CommandNode) commConstructor.newInstance(constant);
-			command.setInput(word);
-			inputSaver.addCommandToHistory(command);
-			command = getVariableOrCommandFromModel(word, command);
-			return command;
+			Class commClass;
+			try {
+				commClass = Class.forName("commands." + commandName);
 
-		} catch (Exception e){
-			e.printStackTrace();
-		}
+				Constructor commConstructor = commClass.getConstructor(double.class);
+							CommandNode command =  (CommandNode) commConstructor.newInstance(constant);
+				command.setInput(word);
+				inputSaver.addCommandToHistory(command);
+				command = getVariableOrCommandFromModel(word, command);
+				return command;
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (NoSuchMethodException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (SecurityException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InstantiationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IllegalArgumentException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InvocationTargetException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		return null;
 	}
 
