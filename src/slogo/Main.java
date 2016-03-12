bf7604da161a93e7166d43fda72140c27cd8af66
Patrick Grady <plg5@duke.edu>
2016-03-11 21:57:35 -0500

    Ugly tabs working, but model is shared between tabs

--- src/slogo/Main.java
+++ src/slogo/Main.java
@@ -6,6 +6,11 @@ import java.util.ResourceBundle;
 
 import gui.GUI;
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -13,19 +18,44 @@ public class Main extends Application {
 	private List<GUI> GUIs = new ArrayList<GUI>();
 	private int currentGUI = 0;
 	private ResourceBundle resources = ResourceBundle.getBundle("resources/Main");
+	private TabPane tabPane = new TabPane();
 
-	@Override
 	public void start(Stage stage) {
+		addGUI();
+
+		BorderPane mainPane = new BorderPane();
+
+
+		mainPane.setCenter(tabPane);
+		mainPane.setTop(addNewTabButton());
+		
+		//mainPane.setCenter(getCurrentGUI().getParent());
+
+		stage.setScene(new Scene(mainPane));
+
+		String title = resources.getString("title");
+		stage.setTitle(title);
+		stage.show();
+	}
+	
+	private Button addNewTabButton()
+	{
+		Button newTabBtn = new Button("New Tab");
+		newTabBtn.setOnAction(e -> addGUI());
+		return newTabBtn;
+	}
+
+	public void addGUI() {
 		int width = Integer.parseInt(resources.getString("width"));
 		int height = Integer.parseInt(resources.getString("height"));
 		String language = resources.getString("default_language");
-		String title = resources.getString("title");
+		
+		GUI gui = new GUI(language, width, height);
+		GUIs.add(gui);
 
-		GUIs.add(new GUI(language, width, height));
-
-		stage.setScene(getCurrentGUI().getScene());
-		stage.setTitle(title);
-		stage.show();
+		Tab tab = new Tab("Workspace");
+		tab.setContent(gui.getParent());
+		tabPane.getTabs().add(tab);
 	}
 
 	public GUI getCurrentGUI() {
