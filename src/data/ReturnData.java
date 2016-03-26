f25cffbaf8c70a9ac26c895db9abe573edb663bb
Patrick Grady <plg5@duke.edu>
2016-03-26 06:21:41 -0400

    Analysis, plg5

--- src/data/ReturnData.java
+++ src/data/ReturnData.java
@@ -1,92 +1,100 @@
+// This entire file is part of my masterpiece.
+// Patrick Grady
+
+/*
+ * This ReturnData class is of my design. It is a way to encalpuslate all the data needed by the
+ * GUI into a compact package that can be passed around easily. Ultimately, it attempts to create a
+ * binding between the front and the backend that is portable (could be adapted to a different frontend)
+ * , easily extendable, and easy to code. This is the only method of data transfer from the backend to the
+ * frontend.
+ * 
+ * In practice, this class held up extremely well. Other interfaces were created to limit the amount of
+ * information that other classes could access, and prevent modification. We never had to change the functionality
+ * of this class for any of the extensions, except debugging, which we didn't implement.
+ */
+
 package data;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
-
 
 public class ReturnData implements CanvasData, WorkspaceData {
 
-    private List<Line> lines = new ArrayList<Line>();
-    private List<Point> turtlePositions = new ArrayList<Point>();
-    private List<TurtleData> turtleInfo;
-    private List<Color> palette;
-    private Image turtleImage;
-    private Map<String, String> userVariables;
-    private Set<String> userFunctions;
-    private double backgroundColor;
-    private List<String[]> history;
+	private List<Line> lines = new ArrayList<Line>();
+	private List<TurtleData> turtleInfo;
+	private List<Color> palette;
+	private Map<String, String> userVariables;
+	private Set<String> userFunctions;
+	private double backgroundColor;
+	private List<String[]> history;
 
-    public ReturnData () {
-    }
+	public List<Line> getLines() {
+		return lines;
+	}
 
-    public List<Line> getLines () {
-        return lines;
-    }
+	public void addLines(List<Line> lines) {
+		this.lines = lines;
+	}
 
-    public void addLines (List<Line> lines) {
-        this.lines = lines;
-    }
+	public double getBackgroundColor() {
+		return backgroundColor;
+	}
 
-    public double getBackgroundColor () {
-        return backgroundColor;
-    }
+	public void setPalette(List<Color> palette) {
+		this.palette = palette;
+	}
 
-    public void setPalette (List<Color> palette) {
-        this.palette = palette;
-    }
+	public Map<String, String> getUserVariables() {
+		return userVariables;
+	}
 
-    public Map<String, String> getUserVariables () {
-        return userVariables;
-    }
+	public void setVariables(Map<String, String> variables) {
+		this.userVariables = variables;
 
-    public void setVariables (Map<String, String> variables) {
-        this.userVariables = variables;
+	}
 
-    }
+	public Set<String> getUserFunctions() {
+		return userFunctions;
+	}
 
-    public Set<String> getUserFunctions () {
-        return userFunctions;
-    }
+	public void setFunctions(Set<String> functionOutputs) {
+		this.userFunctions = functionOutputs;
 
-    public void setFunctions (Set<String> functionOutputs) {
-        this.userFunctions = functionOutputs;
+	}
 
-    }
+	public void setHistory(List<String[]> history) {
+		this.history = history;
+	}
 
-    public void setHistory (List<String[]> history) {
-        this.history = history;
-    }
+	public List<String[]> getHistory() {
+		return this.history;
+	}
 
-    public List<String[]> getHistory () {
-        return this.history;
-    }
+	@Override
+	public List<TurtleData> getTurtles() {
+		return turtleInfo;
+	}
 
-    @Override
-    public List<TurtleData> getTurtles () {
-        return turtleInfo;
-    }
+	public void setTurtles(List<TurtleData> turtles) {
+		this.turtleInfo = turtles;
 
-    public void setTurtles (List<TurtleData> turtles) {
-        this.turtleInfo = turtles;
+	}
 
-    }
+	@Override
+	public List<StampData> getStamps() {
+		return null;
+	}
 
-    @Override
-    public List<StampData> getStamps () {
-        return null;
-    }
+	@Override
+	public List<Color> getPalette() {
+		return palette;
+	}
 
-    @Override
-    public List<Color> getPalette () {
-        return palette;
-    }
-
-    public void setBackgroundColor (double color) {
-        backgroundColor = color;
-    }
+	public void setBackgroundColor(double color) {
+		backgroundColor = color;
+	}
 
 }
--- src/data/StampData.java
+++ src/data/StampData.java
@@ -1,3 +1,6 @@
+// This entire file is part of my masterpiece.
+// Patrick Grady
+
 package data;
 
 public interface StampData {
